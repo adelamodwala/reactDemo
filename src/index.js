7a2b314e87fc1436154f2fda8e443e15341570b9
Adel Amodwala <adel.amodwala@gmail.com>
2016-08-30 21:40:16 -0400

    React Demo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux'
 import { App } from './containers/App';
-import configureStore from './store/configureStore';
-
-const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <div>
-      <App />
-    </div>
-  </Provider>,
+    <App />,
   document.getElementById('root')
 );
